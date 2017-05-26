db25919f4d2c7a774967447c47c0a229ac4e8773
Wojtek Kryscinski <wkryscinski@gmail.com>
2017-05-26 19:14:58 +0200

    Data moving script

---
+++ scripts/reorder-files.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+SRC_DIR=$1
+DEST_DIR=$2
+
+if [[ ! -d $SRC_DIR ]]; then
+    echo "Source path is not a directory..."
+    exit
+fi
+
+if [[ ! -d $DEST_DIR ]]; then
+    echo "Destination path is not a directory..."
+    exit
+fi
+
+echo "Moving audio files to new destination..."
+for filename in $(find $SRC_DIR -type f -name *.flac); do
+    #mv $dir $DEST_DIR
+    echo $filename
+done
+
+echo "Processing text files..."
+for filename in $(find $SRC_DIR -type f -name "*.txt"); do
+    cat $filename | while read line; do
+        FILENAME=$(echo $line | cut -f1 -d ' ')
+        TRANSCRIPTION=$(echo $line | cut -f2- -d ' ')
+        echo -n $TRANSCRIPTION > "$DEST_DIR/$FILENAME.txt"
+    done
+done
